54776d11ae9557182a4f5a66b22033447036feab
truonganh3 <truonga0709@gmail.com>
2025-04-27 09:15:24 +0700

    Initial Commit

---
+++ 2/main.swift
@@ -0,0 +1,44 @@
+//
+//  main.swift
+//  2
+//
+//  Created by MacOS on 27/04/2025.
+//
+//
+//  main.swift
+//  1
+//
+//  Created by MacOS on 27/04/2025.
+//
+
+//import Foundation
+
+//print("Hello, World!")
+//LỆNH ĐIỀU KIỆN
+//if (){
+//else ...
+//else if(){
+//var score = 9
+//if (score > 11){
+//    print("pass")
+//}else if (score > 12){
+//    print("lớn hơn 12")
+//}else if (score == 10){
+//    print("điểm là. 10")
+//}
+//else{
+//    print("điểm của bạn nhỏ hơn 10")
+//}
+var dtb = 5
+//if (dtb >= 5){
+//    print("đây là học sinh khá")
+//}
+//else if (dtb > 8){
+//    print("đây là học sinh giỏi")
+//}
+//else if (dtb < 5){
+//    print("đây là học sinh yếu")
+//}
+// TOÁN TỬ LOGIC AND OR NOT
+//and: &&
+//or
